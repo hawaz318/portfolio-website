ae864356b01325571e3d616525453774dc6f873b
hawaz318 <melkamuayal21@gmail.com>
2025-05-03 21:25:13 +0300

    add mysql in teh about m component

--- Portfolio-Website/src/components/About.jsx
+++ Portfolio-Website/src/components/About.jsx
@@ -44,6 +44,15 @@ const About = ({ isDarkMode}) => {
            </div>
           </div>
       </div>  
+      <div className="flex-items-center">
+           <label htmlFor="htmlandcss" className="w-2/12">MySQL</label>
+           <div className="grow bg-gray-800 rounded-full h-2.5 ">
+             <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5
+              rounded-full transform transition-transform duration-300 hover:scale-105 w-9/12">
+
+             </div>
+           </div>
+          </div>
       </div>
       </div>
       </div>
